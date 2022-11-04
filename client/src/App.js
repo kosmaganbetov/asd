a9d49ace6c5a18f591712f0a43126815f0fc204c
Артур Роднов <artur03rodnov@gmail.com>
2022-11-04 19:43:12 +0600

    fix: img

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TourmateInfo from "./components/TourmateInfo/TourmateInfo";
-import TravellaInfo from "./components/TravellaInfo/TravellaInfo";
-import FooterMain from "./components/FooterMain/FooterMain";
+import MainPage from "./container/MainPage/MainPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TravellaInfo />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
-      <TourmateInfo />
-      <FooterMain />
     </BrowserRouter>
   );
 }
