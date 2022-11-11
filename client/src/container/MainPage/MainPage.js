60fb09e459ca84ba025211a8783d02fe51a44b0d
Артур Роднов <artur03rodnov@gmail.com>
2022-11-11 17:16:02 +0600

    Feat: add online tour

--- client/src/container/MainPage/MainPage.js
+++ client/src/container/MainPage/MainPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import FooterMain from "../../components/MainPage/FooterMain/FooterMain";
 import TourmateInfo from "../../components/MainPage/TourmateInfo/TourmateInfo";
 import TravellaInfo from "../../components/MainPage/TravellaInfo/TravellaInfo";
+import OnlineTours from "../../components/MainPage/OnlineTours/OnlineTours";
 
 const MainPage = () => {
   return (
     <div className="MainPage">
       <TravellaInfo />
       <TourmateInfo />
+      <OnlineTours />
       <FooterMain />
     </div>
   );
