f7cc2750b0181debaf1ad754d0540953f9bb372b
Артур Роднов <artur03rodnov@gmail.com>
2022-12-02 18:05:06 +0600

    Fix: tour page + added map

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,15 +1,12 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
-
-const MapBlock = () => {
-  const defaultState = {
-    center: [55.751574, 37.573856],
-    zoom: 5,
-  };
+import "./Map.sass";
+const MapBlock = (props) => {
   return (
     <div className="MapBlock">
       <YMaps>
-        <Map defaultState={defaultState}>
+        <Map defaultState={props.defaultState} width={"100%"} height={"350px"}>
           <Placemark geometry={[55.684758, 37.738521]} />
         </Map>
       </YMaps>
